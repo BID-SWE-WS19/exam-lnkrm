84fddc11e60d13fdbf5849a2607e739556365790
lnkrm <if18b087@technikum-wien.at>
2019-12-11 15:06:05 +0100

    added test comments to examTestProject

---
+++ CS/examTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace examTestProject
+{
+    public class Tests
+    {
+        // this is a setup method
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        // this is a test method
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
